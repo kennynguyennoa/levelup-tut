0d0706e546f5242e79ccdda45d4deb6ba377d0a2
kennyng90 <kennamngu@gmail.com>
2023-02-19 13:46:42 +0100

    Learned about sequencing hooks and building a logger.
    * Added handleError and handleFetchs in hooks file.
    * Learned how async is working in a function with sequence

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,50 @@
-import { auth, type UserProp } from '$db/fake_auth';
+import { auth } from '$db/fake_auth';
+import type { Handle, HandleFetch } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
 
-type HandleProps = {
+type HandleServerError = (props: {
+	error: Error;
 	event: {
-		locals: {
-			user: UserProp;
-		};
+		request: Request;
+		context: Record<string, unknown>;
+		params: Record<string, unknown>;
 	};
-	resolve: (event: object) => object;
+}) => {
+	status?: string | number;
+	headers?: Record<string, string>;
+	body?: unknown;
+	message?: string;
 };
 
-export async function handle({ event, resolve }: HandleProps) {
+//runs first because of sequence
+const logger: Handle = async ({ event, resolve }) => {
+	const start_time = Date.now();
+	const response = await resolve(event);
+	console.log(`${Date.now() - start_time}ms ${event.request.method} ${event.url.pathname}`);
+
+	return response;
+};
+
+//runs second because of sequence
+const authorize: Handle = ({ event, resolve }) => {
+	console.log('hi');
+
 	const user = auth();
 	event.locals.user = user;
 	return resolve(event);
-}
+};
+
+export const handle: Handle = sequence(logger, authorize);
+
+// Intercept fetch
+export const handleFetch: HandleFetch = ({ fetch, request }) => {
+	return fetch(request);
+};
+
+export const handleError: HandleServerError = ({ error, event }) => {
+	//logger(error, event)
+	return {
+		message: error.message,
+		status: error.message === 'Not found' ? 404 : 500
+	};
+};
