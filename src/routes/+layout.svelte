d0b597f8c5e85441dbe964d480d980e372181eff
kennyng90 <kennamngu@gmail.com>
2023-03-05 22:32:57 +0100

    Learned about browser, build, dev and version imports from $app/environment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,15 @@
 	import Episodes from './Episodes.svelte';
 	import Header from './Header.svelte';
 	import { navigating } from '$app/stores';
+	import { browser, building, dev, version } from '$app/environment';
 
 	export let data: any;
 	$: ({ all_episodes } = data);
+	$: console.log(browser, building, dev, version);
+	//browser: boolean, if app is running in a browser
+	//dev: boolean, true IF running in dev mode
+	//building: boolean, true IF currently build for production
+	//version: string, config.kit.version.name
 </script>
 
 {#if !!$navigating}
